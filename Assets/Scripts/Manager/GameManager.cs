599a98c118bad8e20222034200a7cf95ee2bbf3b
jhhrnavy <jhhrnavy@gmail.com>
2024-04-03 21:58:54 +0900

    feat: stopwatch

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,6 +8,9 @@ public class GameManager : Singleton<GameManager>
     public string thisScene;
     public UIManager uiManager;
 
+    [SerializeField]
+    private Stopwatch _stopwatch;
+
     private void Awake()
     {
         thisScene = SceneManager.GetActiveScene().name;
@@ -23,6 +26,11 @@ public class GameManager : Singleton<GameManager>
         PlayerController.OnPlayerDeath -= PlayerDied;
     }
 
+    private void Start()
+    {
+        _stopwatch.StartStopwatch();
+    }
+
     public void PauseOrContinue()
     {
         Time.timeScale = (Time.timeScale == 1) ? 0f : 1f;
@@ -33,6 +41,10 @@ public class GameManager : Singleton<GameManager>
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(thisScene);
+
+        // Time reset
+        //_stopwatch.StopStopwatch();
+        //_stopwatch.ResetStopwatch();
     }
 
     public void PlayerDied()
